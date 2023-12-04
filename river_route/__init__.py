c5e3d78e8530c770e311f07910734055e4159710
Riley Hales <rileyhales1@gmail.com>
2023-12-03 22:22:21 -0700

    import network trace tools

--- river_route/__init__.py
+++ river_route/__init__.py
@@ -1,5 +1,8 @@
 from river_route._muskingum import Muskingum
+
 from river_route.tools import configs_from_rapid
+from river_route.tools import connectivity_to_digraph
+from river_route.tools import adjacency_matrix
 
 __version__ = '0.3.0'
 __author__ = 'Riley Hales PhD'
@@ -9,6 +12,8 @@ __all__ = [
     'Muskingum',
 
     'configs_from_rapid',
+    'connectivity_to_digraph',
+    'adjacency_matrix',
 
     '__version__',
     '__author__',
