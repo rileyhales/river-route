9cad5b09995176c7410adeb7eb0861e8d5bb1942
rileyhales <rileyhales1@gmail.com>
2024-08-17 09:58:46 -0600

    return after routing

--- river_route/_cli.py
+++ river_route/_cli.py
@@ -18,5 +18,5 @@ def main():
     args = parser.parse_args()
 
     if args.config:
-        rr.MuskingumCunge(args.config).route()
+        return rr.MuskingumCunge(args.config).route()
     return parser.print_help()
